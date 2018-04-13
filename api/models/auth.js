907852d964e1f761dc0b14f8d78775d903048db8
Huy Nguyen <huy@HuyPC.WORKGROUP>
2018-04-13 16:14:41 +0700

    add auth

--- api/models/auth.js
+++ api/models/auth.js
@@ -4,7 +4,6 @@ var jtoken = require('jsonwebtoken');
 var config = require('../../config.js');
 
 exports.createToken = function(mac){
-    console.log(mac);
     var token = jtoken.sign(mac,config.secret);
     return token;
 }
--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
     secret : 'UiTiOt',
     ip : 'http://103.88.123.103:8888',
-    mqtt_host : '',
-    mqtt_user : 'test',
-    mqtt_password : 'test',
+    mqtt_host : '103.88.123.103',
+    mqtt_user : 'root',
+    mqtt_password : 'root',
     mongo_URL : "mongodb://localhost:27017/test",
-    
+    mqtt_topic : "xiaomi",
+    mqtt_key : "xiaomi",
+    mqtt_port : 8883
+
 }
