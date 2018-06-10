2daf547fbdf45091ba14c4a156fcad3abaf37e4e
Huy Nguyen <14520368@gm.uit.edu.vn>
2018-06-10 10:11:52 +0700

    new

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     secret : 'UiTiOt',
-    ip : 'http://192.168.1.110:8888',
+    ip : 'http://171.244.28.106:8888',
     mqtt_host : '103.88.123.103',
     mqtt_user : 'root',
     mqtt_password : 'root',
     mongo_URL : "mongodb://localhost:27017/test",
-    mqtt_topic_test : "xiaomi/test",
-    mqtt_topic_result:"xiaomi/result",
+    mqtt_topic_test : "xiaomi/test/",
+    mqtt_topic_result:"xiaomi/result/",
     mqtt_key : "xiaomi",
     mqtt_port : 8883,
 
